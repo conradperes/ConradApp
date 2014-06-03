4bdaefadf6c5298c537851195a6b2bb4b0278bed
Conrad Marques Peres <conradmarquesperes@MacBook-Pro-de-Conrad.local>
2014-06-03 20:37:05 -0300

    Initial Commit

---
+++ ConradApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ConradApp
+//
+//  Created by Conrad Marques Peres on 03/06/14.
+//  Copyright (c) 2014 Esparta Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ConradAppTests/ConradAppTests.m
@@ -0,0 +1,34 @@
+//
+//  ConradAppTests.m
+//  ConradAppTests
+//
+//  Created by Conrad Marques Peres on 03/06/14.
+//  Copyright (c) 2014 Esparta Tech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ConradAppTests : XCTestCase
+
+@end
+
+@implementation ConradAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
